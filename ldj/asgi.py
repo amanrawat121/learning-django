51680048392040fdfa73d81d3677113c5d68ea06
Aman Rawat <neogamers21@gmail.com>
2023-08-25 07:38:03 +0530

    initial commit for ldj repo

---
+++ ldj/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ldj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ldj.settings')
+
+application = get_asgi_application()
