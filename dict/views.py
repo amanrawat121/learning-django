fae5b2f3be7597e96ec016c70053e5e61b8a6c6e
Aman Rawat <neogamers21@gmail.com>
2023-09-14 17:27:28 +0530

    removed try except blocks

--- dict/views.py
+++ dict/views.py
@@ -7,16 +7,16 @@ from django.views import View
 class DictionaryView(View):
     async def fetch_data(self, word):
         baseurl = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
-        try:
-            async with ClientSession() as session:
-                async with session.get(baseurl) as response:
-                    if response.status == 200:
-                        data = await response.json()
-                        return JsonResponse(data[0])
-                    else:
-                        return HttpResponse("Can't find the meaning of this", status=404)
-        except Exception as e:
-            return HttpResponse("An error occurred: " + str(e), status=500)
+        # try:
+        async with ClientSession() as session:
+            async with session.get(baseurl) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return JsonResponse(data[0])
+                else:
+                    return HttpResponse("Can't find the meaning of this", status=404)
+        # except Exception as e:
+        #     return HttpResponse("An error occurred: " + str(e), status=500)
 
     async def get(self, request, word):
         data = await self.fetch_data(word)
