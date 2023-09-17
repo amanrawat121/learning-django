df98771df950451529b8f6450be571c653d6cd65
Aman Rawat <neogamers21@gmail.com>
2023-09-17 19:26:19 +0530

    autocomplete off in search box

--- dict/urls.py
+++ dict/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
-from .views import DictionaryView, sample
+from .views import *
+
 urlpatterns = [
+    path("", home, name="home"),
     re_path(r"define/(?P<word>[a-zA-Z]+)/", DictionaryView.as_view(), name="dict"),
-    path("sample", sample, name="sample")
 ]
